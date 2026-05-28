5a1c3a98d487a953fe8ac21f285e6e8988f1f537
jemmyprestes <jemmyprestes@gmail.com>
2026-05-28 09:56:07 +0200

    Update generate-course.js

--- api/generate-course.js
+++ api/generate-course.js
@@ -1,4 +1,15 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  // resto do teu código...
+}
+
   // CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
