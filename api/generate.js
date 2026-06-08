b0d5c9a3a8f37b881157eb8c76873a9891d3992f
jemmyprestes <jemmyprestes@gmail.com>
2026-06-08 17:49:12 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
+  // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
@@ -9,6 +10,7 @@ export default async function handler(req, res) {
 
   const { topic } = req.body;
 
+  // Aqui você coloca sua lógica real depois
   res.status(200).json({
     course: `Curso gerado com sucesso para o tópico: ${topic}`
   });
